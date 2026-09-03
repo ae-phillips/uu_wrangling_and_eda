683cf798c9de35f9ee6850922553498c2a0aeb00
ae-phillips <153021862+ae-phillips@users.noreply.github.com>
2026-09-03 18:15:24 -0400

    create separate mapping file for giftor names to preserve space

---
+++ lab_1/giftor_mapping.py
@@ -0,0 +1,186 @@
+giftor_map = {
+    # Bangladesh
+    "Intl Ctr for Diarrhoeal Disease": "International Centre for Diarrhoeal Disease",
+    "Intl. Cntr for Diarrihoeal Disease": "International Centre for Diarrhoeal Disease",
+    "Intnl Centre for Diarrhoeal Disease": "International Centre for Diarrhoeal Disease",
+    "Intrnl Centre Diarrheal Disease": "International Centre for Diarrhoeal Disease",
+    "International Centre for Diarrhoeal": "International Centre for Diarrhoeal Disease",
+    # DENMARK
+    "Novo Nordisk A/S": "Novo Nordisk",
+    # England
+    "GW Research Ltd": "GW Research Ltd.",
+    "G. W. Research Ltd": "GW Research Ltd.",
+    "TauRx Therapeutics Ltd": "TauRx Therapeutics Ltd.",
+    "Charles Aid Foundation": "Charities Aid Foundation",  # confirmed that this doesn't exist
+    # India
+    "Indian Inst of Tropical Meteorology": "Indian Institute of Tropical Meteorology",
+    "Indian Institute of Tropical Meteor": "Indian Institute of Tropical Meteorology",
+    # Israel
+    "InSightec-Image Guide Treatment Ltd": "InSightec-Image Guided Treatment Ltd",
+    "InSightec-Image Guided Treatment Lt": "InSightec-Image Guided Treatment Ltd",
+    # South Korea
+    "Daegu Gyeongbuk Inst of Sc& Tech": "Daegu Gyeongbuk Inst of Sci & Tech",
+    # Switzerland
+    "Lawrence D/ Karin S Howell": "Lawrence D & Karin S Howell",
+    # Tanzania
+    "Haydom Luther Hospital": "Haydom Lutheran Hospital",
+    "Haydom Lutheran Hospital d/b/a Hayd": "Haydom Lutheran Hospital",
+    # -------------------------
+    # Saudi Arabian Cultural Mission (SACM)
+    # -------------------------
+    "Saudi Arabia Education Mission": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Education Mission,": "Saudi Arabian Cultural Mission",
+    "Saudia Arabian Cultural Mission": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Mission - US": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Mission to t": "Saudi Arabian Cultural Mission",
+    "Cultural Mission/Royal Embassy": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Mission & Sa": "Saudi Arabian Cultural Mission",
+    "SACM": "Saudi Arabian Cultural Mission",
+    "SACM (Tuition and Fees Payments)": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Missions": "Saudi Arabian Cultural Mission",
+    "Saudi arabian Cultural Mission": "Saudi Arabian Cultural Mission",
+    "The Saudi Arabian Cultural Mission": "Saudi Arabian Cultural Mission",
+    "SACM - Fall 2018": "Saudi Arabian Cultural Mission",
+    "SACM - Spring 2019": "Saudi Arabian Cultural Mission",
+    "SACM - Summer II 2018": "Saudi Arabian Cultural Mission",
+    "SACM - Summer I 2019": "Saudi Arabian Cultural Mission",
+    "SAUDI ARABIAN CULTURAL MISSION": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultutal Mission": "Saudi Arabian Cultural Mission",
+    "Saudi Arabia Cultural Mission-SACM": "Saudi Arabian Cultural Mission",
+    "SACM Saudi Arabia Cultrual Mission": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Misson": "Saudi Arabian Cultural Mission",
+    "Saudi Arabia Cultural Mission (SACM": "Saudi Arabian Cultural Mission",
+    "Saudi Arabian Cultural Missian": "Saudi Arabian Cultural Mission",
+    "Saudii Arabian Cultural Mission (SA": "Saudi Arabian Cultural Mission",
+    "Royal Embassy Saudi Cultural Missio": "Saudi Arabian Cultural Mission",
+    "Cul Mission of the Royal Embassy SA": "Saudi Arabian Cultural Mission",
+    "Cultural Mission of the Royal Embas": "Saudi Arabian Cultural Mission",
+    "Royal Embassy of Saudi Arabia - Cul": "Saudi Arabian Cultural Mission",
+    # Keep this separate if you want embassy itself distinct
+    "Royal Embassy of Saudi Arabia/SACM": "Saudi Arabian Cultural Mission",
+    # -------------------------
+    # KAUST
+    # King Abdullah University of Science and Technology
+    # -------------------------
+    "KING ABDULLAH UNIVERSITY FOR SCIENC": "King Abdullah University of Science and Technology",
+    "King Abdullah University for S&T": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Sci & Tech": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIV SCIENCE AND TECH": "King Abdullah University of Science and Technology",
+    "King Abdullah University of Science": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIVERSITY": "King Abdullah University of Science and Technology",
+    "King ABDULLAH UNIVERSITY": "King Abdullah University of Science and Technology",
+    "King Abdula Univ of Science & Tech": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIVERSITY OF SCIENCE": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Science & Tec": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIV OF SCI & TECH (K": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Sci and Tech": "King Abdullah University of Science and Technology",
+    "KAUST": "King Abdullah University of Science and Technology",
+    "IIE - KAUST": "King Abdullah University of Science and Technology",
+    "King Abdullah University Sci & Tech": "King Abdullah University of Science and Technology",
+    "King Abdullah U of Sci & Tech": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Science -Tech": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Science and T": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ of Science Tech": "King Abdullah University of Science and Technology",
+    "King Abdullah Univ for Sci & Tech": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIV FOR SCI & TECH (": "King Abdullah University of Science and Technology",
+    "KING ABDULLAH UNIV OF SCIENCE AND T": "King Abdullah University of Science and Technology",
+    # -------------------------
+    # King Fahd University of Petroleum & Minerals
+    # -------------------------
+    "KING FAHD UNIVERSITY OF PETROLEUM &": "King Fahd University of Petroleum and Minerals",
+    "King Fahd Univ of Petroleum and Min": "King Fahd University of Petroleum and Minerals",
+    "King Fahd U of Petroleum & Minerals": "King Fahd University of Petroleum and Minerals",
+    "KING FAHD UNIV OF PETRO AND MIN": "King Fahd University of Petroleum and Minerals",
+    "King Fahd Univ of Petr & Mineral": "King Fahd University of Petroleum and Minerals",
+    "KING FAHD UNIV OF PETROLEUM AND MIN": "King Fahd University of Petroleum and Minerals",
+    # -------------------------
+    # King Abdulaziz City for Science and Technology (KACST)
+    # -------------------------
+    "King Abdulaziz City for Sci & Tech": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City For S and T": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City for Science and": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City for Sci and Tec": "King Abdulaziz City for Science and Technology",
+    "KING ABDULAZIZ CITY FOR SCIENCE AND": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City for Science": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City Sci & Tech": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City Science & Techn": "King Abdulaziz City for Science and Technology",
+    "King Abdulaziz City for Sci Tech": "King Abdulaziz City for Science and Technology",
+    # -------------------------
+    # Saudi Aramco / Aramco
+    # -------------------------
+    "Aramco": "Saudi Aramco",
+    "ARAMCO": "Saudi Aramco",
+    "Saudi Arabia ARAMCO": "Saudi Aramco",
+    "Saudi Arabian Oil Company (ARAMCO)": "Saudi Aramco",
+    "SAUDI ARAMCO": "Saudi Aramco",
+    # -------------------------
+    # Aramco Services Company
+    # -------------------------
+    "Aramco Services Company": "Aramco Services Company",
+    "ARAMCO SERVICES COMPANY": "Aramco Services Company",
+    "ARAMCO SERVICES COMPANY/DHAHRAN,SAU": "Aramco Services Company",
+    "ARAMCO Services": "Aramco Services Company",
+    "Armaco Services Company": "Aramco Services Company",
+    "Aramco Services": "Aramco Services Company",
+    # -------------------------
+    # Aramco Gulf Operations
+    # -------------------------
+    "Armaco Gulf Operations Company": "Aramco Gulf Operations Company",
+    "Aramco Gulf Operations Company": "Aramco Gulf Operations Company",
+    # -------------------------
+    # SABIC
+    # -------------------------
+    "Saudi Basic Industries Corp (SABIC)": "Saudi Basic Industries Corporation",
+    "Saudi Basic Industries Corporation": "Saudi Basic Industries Corporation",
+    "Saudi Basic Industries Corp.": "Saudi Basic Industries Corporation",
+    # SABIC Americas
+    "SABIC Americas, Inc.": "SABIC Americas, Inc.",
+    "Sabic Americas Inc.": "SABIC Americas, Inc.",
+    "Sabic Americas Inc": "SABIC Americas, Inc.",
+    "Sabic Americas, Inc.": "SABIC Americas, Inc.",
+    # -------------------------
+    # King Saud University
+    # -------------------------
+    "KING SAUD UNIVERSITY": "King Saud University",
+    # -------------------------
+    # King Abdulaziz University
+    # -------------------------
+    "KING ABDULAZIZ UNIVERSITY": "King Abdulaziz University",
+    # -------------------------
+    # Saudi Ministry of Education
+    # -------------------------
+    "Saudi Arabia Ministry of Education": "Saudi Arabia Ministry of Education",
+    "Saudi Arabian Ministry of Education": "Saudi Arabia Ministry of Education",
+    # -------------------------
+    # Ministry of Higher Education
+    # -------------------------
+    "Ministry of Higher Ed, Kingdom of S": "Saudi Arabia Ministry of Higher Education",
+    "Ministry of Higher Education": "Saudi Arabia Ministry of Higher Education",
+    # -------------------------
+    # Ministry of Defense
+    # -------------------------
+    "Saudi Arabian Ministry of Defense a": "Saudi Arabia Ministry of Defense",
+    "SAUDI ARADBIAN MINISTRY OF DEFFENSE": "Saudi Arabia Ministry of Defense",
+    "Suadi Arabia Ministry of Defense": "Saudi Arabia Ministry of Defense",
+    # -------------------------
+    # Embassy
+    # -------------------------
+    "Embassy of Saudi Arabia": "Royal Embassy of Saudi Arabia",
+    "Embassy of Saudi": "Royal Embassy of Saudi Arabia",
+    # -------------------------
+    # Government / Kingdom
+    # -------------------------
+    "KINGDOM OF SAUDI ARABIA": "Kingdom of Saudi Arabia",
+    # -------------------------
+    # Dhahran Techno Valley
+    # -------------------------
+    "Dhahran Techno Valley Comoany": "Dhahran Techno Valley Company",
+    "Dhahran Techno-Valley Co.": "Dhahran Techno Valley Company",
+    # -------------------------
+    # Abdulrahman R. Al-Rashed
+    # -------------------------
+    "Mr. Abdulrahman R. Al-Rashed": "Abdulrahman R. Al-Rashed",
+    "Embassy of the State of Kuwait": "Embassy of Kuwait",
+    # Netherlands
+    "SABIC Global Tecnologies BV": "SABIC Global Technologies BV",
+}
