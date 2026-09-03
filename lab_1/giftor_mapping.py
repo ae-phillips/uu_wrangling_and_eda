a1458b1306849f1442be5f62347e60e62a025a63
ae-phillips <153021862+ae-phillips@users.noreply.github.com>
2026-09-03 18:25:43 -0400

    updates to giftor mapping (china)

--- lab_1/giftor_mapping.py
+++ lab_1/giftor_mapping.py
@@ -31,6 +31,7 @@ giftor_map = {
     "Saudi Arabia Education Mission": "Saudi Arabian Cultural Mission",
     "Saudi Arabian Education Mission,": "Saudi Arabian Cultural Mission",
     "Saudia Arabian Cultural Mission": "Saudi Arabian Cultural Mission",
+    "Saudi Arabia Cultural Mission": "Saudi Arabian Cultural Mission",
     "Saudi Arabian Cultural Mission - US": "Saudi Arabian Cultural Mission",
     "Saudi Arabian Cultural Mission to t": "Saudi Arabian Cultural Mission",
     "Cultural Mission/Royal Embassy": "Saudi Arabian Cultural Mission",
@@ -183,4 +184,226 @@ giftor_map = {
     "Embassy of the State of Kuwait": "Embassy of Kuwait",
     # Netherlands
     "SABIC Global Tecnologies BV": "SABIC Global Technologies BV",
+    # -------------------------
+    # China Scholarship Council
+    # -------------------------
+    "Chinese Scholarship Council": "China Scholarship Council",
+    "China Scholarship Council (Scholars": "China Scholarship Council",
+    # -------------------------
+    # Huawei Technologies
+    # -------------------------
+    "Huawei Technologies": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co Ltd": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co, LTD": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co., Ltd": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co. Ltd": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co.": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies, Ltd.": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies, Co., Ltd": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Co, Ltd": "Huawei Technologies Co., Ltd.",
+    "Huawei Technologies Company Inc": "Huawei Technologies Co., Ltd.",
+    "HUAWEI TECHNOLOGIES CO": "Huawei Technologies Co., Ltd.",
+    "HUAWEI TECHNOLOGIES CO.": "Huawei Technologies Co., Ltd.",
+    # I'd keep Huawei Technologies USA separate unless
+    # you explicitly want parent-company consolidation.
+    # -------------------------
+    # Confucius Institute / Hanban
+    # -------------------------
+    "CONFUCIUS INSTITUTE HEADQUARTERS": "Confucius Institute Headquarters",
+    "CONFUCIOUS INSTITUTE HEADQUARTERS": "Confucius Institute Headquarters",
+    "Confucius Inst. Headquarters": "Confucius Institute Headquarters",
+    "CONFUCIUS INST HEADQUARTERS": "Confucius Institute Headquarters",
+    "Headquarters of Confucius Institute": "Confucius Institute Headquarters",
+    "Confucius Inst. HQ of China": "Confucius Institute Headquarters",
+    "Confucius Institute Hd of China": "Confucius Institute Headquarters",
+    "Confucious Institute HQ of China": "Confucius Institute Headquarters",
+    "Confucius Institute HQ of China": "Confucius Institute Headquarters",
+    "Confucius Institute- Hanban Headqua": "Confucius Institute Headquarters",
+    # Broader Hanban / Confucius Institute naming
+    "Hanban": "Hanban / Confucius Institute",
+    "Hanban, China": "Hanban / Confucius Institute",
+    "HANBAN China": "Hanban / Confucius Institute",
+    "HanBan/Confucius Institute": "Hanban / Confucius Institute",
+    "Confucius Inst. Hanban": "Hanban / Confucius Institute",
+    "Hanban Confucius Institute": "Hanban / Confucius Institute",
+    "Hanban: Confucius Institute": "Hanban / Confucius Institute",
+    "Hanban-Confucius Institute": "Hanban / Confucius Institute",
+    "Confucious Institute": "Hanban / Confucius Institute",
+    # -------------------------
+    # Tsinghua University
+    # -------------------------
+    "TSINGHUA UNIVERSITY": "Tsinghua University",
+    # -------------------------
+    # Zhejiang University
+    # -------------------------
+    "ZHEJIANG UNIVERSITY": "Zhejiang University",
+    "Zhejian University": "Zhejiang University",
+    # -------------------------
+    # Beijing Normal University
+    # -------------------------
+    "Pejing Normal University": "Beijing Normal University",
+    "Bejing Normal University": "Beijing Normal University",
+    # -------------------------
+    # Shanghai Maritime University
+    # -------------------------
+    "SHANGHAI MARITIME UNIVERSITY": "Shanghai Maritime University",
+    # -------------------------
+    # Guizhou University of Finance & Economics
+    # -------------------------
+    "Guizhou Univ of Finance and Econ": "Guizhou University of Finance & Economics",
+    "Guizhou University of Finance and E": "Guizhou University of Finance & Economics",
+    # -------------------------
+    # Jiangxi University of Finance & Economics
+    # -------------------------
+    "Jiangxi Univesity of Finance & Eco": "Jiangxi University of Finance & Economics",
+    # -------------------------
+    # CRRC Industrial Institute
+    # -------------------------
+    "CRRC Industrial Institue Co Ltd.": "CRRC Industrial Institute Co., Ltd.",
+    "CRRC Industrial Institute": "CRRC Industrial Institute Co., Ltd.",
+    "CRRC Industrial Institute Co Ltd": "CRRC Industrial Institute Co., Ltd.",
+    "CRRC Industrial Institute Co LTD": "CRRC Industrial Institute Co., Ltd.",
+    "CRRD Industrial Institute Co": "CRRC Industrial Institute Co., Ltd.",
+    "CRRC Industrial Institute Co": "CRRC Industrial Institute Co., Ltd.",
+    "CRCC Industrial Institute Co., LTD": "CRRC Industrial Institute Co., Ltd.",
+    # -------------------------
+    # Beijing Guanghui Jintong Education
+    # -------------------------
+    "Beijing Guanghui Jinton Education": "Beijing Guanghui Jintong Education",
+    "Beijiing Guanghui Jintong Education": "Beijing Guanghui Jintong Education",
+    "Beijing Guanghui Jintong Educatino": "Beijing Guanghui Jintong Education",
+    # -------------------------
+    # Educational Foundation of USTC
+    # -------------------------
+    "The Education Foundation of USTC": "Educational Foundation of USTC",
+    "The Educational Foundation USTC": "Educational Foundation of USTC",
+    "The Educational Foundation of USTC": "Educational Foundation of USTC",
+    # -------------------------
+    # Guiyang Berkeley Big Data Institute
+    # -------------------------
+    "Guiyang Berkeley Big Data Inst": "Guiyang Berkeley Big Data Institute",
+    # -------------------------
+    # Ant Financial
+    # -------------------------
+    "Art Financial Services Group": "Ant Financial Services Group",
+    # -------------------------
+    # BOE Technology Group
+    # -------------------------
+    "BOE Technology Group Company": "BOE Technology Group Co., Ltd.",
+    "BOE Technology": "BOE Technology Group Co., Ltd.",
+    # -------------------------
+    # Futurewei
+    # -------------------------
+    "FutureWei Technologies Inc": "Futurewei Technologies, Inc.",
+    "Futurewei": "Futurewei Technologies, Inc.",
+    # -------------------------
+    # ReStem China
+    # -------------------------
+    "ReStem China Co Ltd": "ReStem China Co. Ltd",
+    # -------------------------
+    # CalyGene Biotechnology
+    # -------------------------
+    "CalyGene Biotechnololgy": "CalyGene Biotechnology",
+    "Hangzhou Calygene Biotechnology Co.": "CalyGene Biotechnology",
+    # -------------------------
+    # Changan Automobile
+    # -------------------------
+    "Changan Automobile Co., LTD": "Changan Automobile Co., Ltd.",
+    # -------------------------
+    # Shanghai Nuclear Engineering R&D
+    # -------------------------
+    "Shanghai Nuclear Eng Research & Des": "Shanghai Nuclear Engineering R&D",
+    # -------------------------
+    # Beijing Institute of Collaborative Innovation
+    # -------------------------
+    "BEIJING INSTITUTE OF COLLABORATIVE": "Beijing Institute of Collaborative Innovation",
+    "Beijing Inst of Collaborative Innov": "Beijing Institute of Collaborative Innovation",
+    "Beijing inst of Collab Innovation": "Beijing Institute of Collaborative Innovation",
+    # -------------------------
+    # Infinitus
+    # -------------------------
+    "Infinitus Company Ltd": "Infinitus (China) Company Ltd.",
+    # -------------------------
+    # ME Genomics
+    # -------------------------
+    "ME GENOMICS": "ME Genomics",
+    # -------------------------
+    # Progressive Education Foundation
+    # -------------------------
+    "Progressive Education Foundation Li": "Progressive Education Foundation",
+    # -------------------------
+    # Cheung Kong Graduate School of Business
+    # -------------------------
+    "Cheung Kong Graduate School of Bus": "Cheung Kong Graduate School of Business",
+    # -------------------------
+    # China Merchants Bank
+    # -------------------------
+    "CHINA MERCHANTS BANK": "China Merchants Bank",
+    # -------------------------
+    # China Everbright Group
+    # -------------------------
+    "CHINA EVERBRIGHT GROUP": "China Everbright Group",
+    # -------------------------
+    # China Minsheng Bank
+    # -------------------------
+    "CHINA MINSHENG BANK CO. LTD.": "China Minsheng Banking Co., Ltd.",
+    # -------------------------
+    # Mary Kay China
+    # -------------------------
+    "MARY KAY(CHINA) CO.,LTD": "Mary Kay (China) Co., Ltd.",
+    # -------------------------
+    # E-House China
+    # -------------------------
+    "E-HOUSE(CHINA) HOLDINGS UNLIMITED": "E-House (China) Holdings Limited",
+    "E-House (China) Holdings Unlimited": "E-House (China) Holdings Limited",
+    # -------------------------
+    # Guangzhou R&F Properties
+    # -------------------------
+    "GUANGHZHOU R&F PROPERTIES CO., LTD.": "Guangzhou R&F Properties",
+    "Guanghzhou R&F Properties Co., Ltd.": "Guangzhou R&F Properties",
+    "GUANGHZHOU R&F PROPERTIESCO.,LTD": "Guangzhou R&F Properties",
+    # -------------------------
+    # Moelis & Company
+    # -------------------------
+    "MOELIS &COMPANY": "Moelis & Company",
+    "MOELIS & COMPANY": "Moelis & Company",
+    # -------------------------
+    # BGP
+    # -------------------------
+    "BGp Inc.": "BGP Inc.",
+    "BGP INc.": "BGP Inc.",
+    # -------------------------
+    # Huadong Pharmaceuticals
+    # -------------------------
+    "Huadong Pharmeceuticals": "Huadong Pharmaceuticals",
+    # -------------------------
+    # Ocean University of China
+    # -------------------------
+    "Ocen University of China": "Ocean University of China",
+    # -------------------------
+    # China University of Mining & Technology
+    # -------------------------
+    "China University of Mining and Tech": "China University of Mining & Technology",
+    # -------------------------
+    # Southwest Jiaotong University
+    # -------------------------
+    "-Southwest Jiaotong University": "Southwest Jiaotong University",
+    # -------------------------
+    # Personal-name formatting duplicates
+    # -------------------------
+    '"LIANG, YAN"': "LIANG, YAN",
+    "KANG, PEI": "Kang, Pei",
+    "BIAN, JIWU": "Bian, Jiwu",
+    "SUN, GANG": "Sun, Gang",
+    "SHIH, STEPHEN": "Shih, Stephen",
+    "LI, BIN": "Li, Bin",
+    "PENG, JUN": "Peng, Jun",
+    "LIU, YI": "Liu, Yi",
+    "LIU, GE": "Liu, Ge",
+    "WU, LIANG": "Wu, Liang",
+    "YAN, JIAYONG": "Yan, Jiayong",
+    "YU, XINQI": "Yu, Xinqi",
+    "ZHANG, YONGQIAN": "Zhang, Yongqian",
+    "ZHU, WENBIN": "Zhu, Wenbin",
 }
